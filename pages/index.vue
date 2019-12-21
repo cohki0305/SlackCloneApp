2af0c313ae830d02d6f14dbf44ce09b797facd89
cohki0305 <cohki0305@gmail.com>
2019-12-21 18:58:21 +0900

    slack っぽい UI を作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="container">
+    <div class="chats-layout">
+      <messages />
+    </div>
+    <div class="input-layout">
+      <chat-form />
+    </div>
   </div>
 </template>
 
 <script>
+import Messages from '~/components/Messages.vue'
+import ChatForm from '~/components/ChatForm.vue'
 
 export default {
   components: {
+    Messages,
+    ChatForm
   }
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  height: 100%;
+}
+
+.chats-layout {
+  overflow: scroll;
+  height: 90%;
+}
+
+.input-layout {
+  height: 10%;
+}
 </style>

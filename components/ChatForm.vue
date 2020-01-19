372dd39748a8fc2fd21d02803be2f97e62b5f07a
cohki0305 <cohki0305@gmail.com>
2020-01-19 10:39:34 +0900

    ログインモーダルでログインを実行

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -2,16 +2,18 @@
   <div class="input-container">
     <textarea v-model="text" v-on:click="openLoginModal" v-on:keydown.enter="addMessage"></textarea>
     <el-dialog
-      title="Tips"
+      title=""
       :visible.sync="dialogVisible"
       width="30%">
-      <span>This is a message</span>
+      <div class="image-container">
+        <img src="~/assets/google_sign_in.png" v-on:click="login"/>
+      </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { db } from '~/plugins/firebase'
+import { db, firebase } from '~/plugins/firebase'
 
 import Vue from 'vue'
 import ElementUI from 'element-ui'
@@ -42,6 +44,16 @@ export default {
     keyDownedForJPConversion (event) {
       const codeForConversion = 229
       return event.keyCode === codeForConversion
+    },
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user
+          console.log(user)
+        }).catch((error) => {
+          window.alert(error)
+        })
     }
   }
 }
@@ -56,5 +68,15 @@ export default {
 textarea {
   width: 100%;
   height: 100%;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  width: 70%;
+  cursor: pointer;
 }
 </style>
